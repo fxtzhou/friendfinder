b1b440e65ae0856adc3ff717bc80f1ffa51c0d98
Fiona Zhou <fxtzhou@yahoo.com>
2019-09-14 19:34:02 -0700

    deploying heroku

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [{
+        name: "Fiona",
+        instagram: "https://www.stickpng.com/assets/images/580b585b2edbce24c47b29f6.png",
+        scores: ["5", "2", "1", "5", "3", "1", "1", "5", "4", "1"]
+    },
+    {
+        name: "Dio",
+        instagram: "https://66.media.tumblr.com/ba8684a7df1af67243ada8b5890823ba/tumblr_pmsciwZ98V1v7ql19_540.jpg",
+        scores: ["3", "3", "3", "3", "3", "3", "2", "3", "3", "3"]
+    },
+    {
+        name: "Kylo Ren",
+        instagram: "https://images.pexels.com/photos/34617/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    }
+];
+
+module.exports = friends;
